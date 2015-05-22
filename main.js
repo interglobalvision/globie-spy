3e52a1361e1d30558bd04964b270a3c7974c8dff
Cas <casska@gmail.com>
2015-05-22 15:14:00 -0500

    add - reset working

--- main.js
+++ main.js
@@ -12,41 +12,75 @@ app.get('/:direction', function (req, res) {
   var direction = req.params.direction,
       cmd = 'uvcdynctrl';
   
-  switch(direction) {
-    case 'up':
-      cmd += " -s 'Tilt (relative)' -- 1000";
-      break;
-    case 'down':
-      cmd += " -s 'Tilt (relative)' -- -1000";
-      break;
-    case 'left':
-      cmd += " -s 'Pan (relative)' -- 1000";
-      break;
-    case 'right':
-      cmd += " -s 'Pan (relative)' -- 1000";
-      break;
-    case 'reset':
-      cmd += " -s 'Pan reset' && " + cmd + " -s 'Tilt reset'"; 
-      break;
+  if(direction === "reset") {
+
+    child = exec(cmd + " -s 'Pan Reset' 1", function (error, stdout, stderr) {
+      console.log('Pan Reset');
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        res.json({
+          'success': 'false',
+          'error': 'exec error: ' + error
+        }) 
+      } else {
+        setTimeout( function() {
+          child = exec(cmd + " -s 'Tilt Reset' 1", function (error, stdout, stderr) {
+            console.log('Tilt Reset');
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if (error !== null) {
+              console.log('exec error: ' + error);
+              res.json({
+                'success': 'false',
+                'error': 'exec error: ' + error
+              }) 
+            } else {
+              res.json({
+                'success': 'true'
+              }) 
+            } 
+          });
+        }, 2000); 
+      } 
+    }); 
+
+  } else {
+  
+    switch(direction) {
+      case 'up':
+        cmd += " -s 'Tilt (relative)' -- 1000";
+        break;
+      case 'down':
+        cmd += " -s 'Tilt (relative)' -- -1000";
+        break;
+      case 'left':
+        cmd += " -s 'Pan (relative)' -- -1000";
+        break;
+      case 'right':
+        cmd += " -s 'Pan (relative)' -- 1000";
+        break;
+    }
+
+    console.log(cmd);
+
+    child = exec(cmd, function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        res.json({
+          'success': 'false',
+          'error': 'exec error: ' + error
+        }) 
+      } else {
+        res.json({
+          'success': 'true'
+        }) 
+      } 
+    }); 
   }
-
-  console.log(cmd);
-
-  child = exec(cmd, function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-      res.json({
-        'success': 'false',
-        'error': 'exec error: ' + error
-      }) 
-    } else {
-      res.json({
-        'success': 'true'
-      }) 
-    } 
-  }); 
 });
 
 var server = app.listen(8083, function () {
