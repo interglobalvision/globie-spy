55563b615772fb95c9eb7f6f4255be3738f2b1e0
Globie RPi <globie@interglobal.vision>
2016-10-06 16:36:05 +0000

    add limit to cam movement

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var app = express();
 
-var exec = require('child_process').exec,
-    child;
+var exec = require('child_process').exec, child;
 
+var step = 300;
+var limit = {
+  x: 2,
+  y: 2,
+};
+
+var position = { 
+  x: 0,
+  y: 0,
+};
+
+var resetPosition = function(callback) {
+  child = exec("uvcdynctrl -s 'Pan Reset' 1", function (error, stdout, stderr) {
+    console.log('Pan Reset');
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+
+    if (error !== null) { // Error
+      console.log('exec error: ' + error);
+
+    } else { // Success
+      position.x = 0;
+
+      setTimeout( function() {
+        child = exec("uvcdynctrl -s 'Tilt Reset' 1", function (error, stdout, stderr) {
+          console.log('Tilt Reset');
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+
+          if (error !== null) { // ERROR
+            console.log('exec error: ' + error);
+          } else { // SUCCESS
+            position.y = 0;
+          }
+        });
+
+      }, 3500); 
+    } 
+  }); 
+};
+
+/* Set CORS */
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -16,15 +57,8 @@ app.get('/', function (req, res) {
 
 app.post('/api/:direction', function (req, res) {
 
-  /* Set CORS */
-/*
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-*/
-
-  var direction = req.params.direction,
-      cmd = 'uvcdynctrl';
+  var direction = req.params.direction;
+  var cmd = 'uvcdynctrl';
   
   if(direction === "reset") {
 
@@ -35,7 +69,7 @@ app.post('/api/:direction', function (req, res) {
       if (error !== null) {
         console.log('exec error: ' + error);
         res.json({
-          'success': 'false',
+          'success': false,
           'error': 'exec error: ' + error
         }) 
       } else {
@@ -47,7 +81,7 @@ app.post('/api/:direction', function (req, res) {
             if (error !== null) {
               console.log('exec error: ' + error);
               res.json({
-                'success': 'false',
+                'success': false,
                 'error': 'exec error: ' + error
               }) 
             } else {
@@ -64,36 +98,56 @@ app.post('/api/:direction', function (req, res) {
   
     switch(direction) {
       case 'up':
-        cmd += " -s 'Tilt (relative)' -- -100";
+        if (position.y < limit.y) {
+          position.y += 1;
+          cmd += " -s 'Tilt (relative)' -- -" + step;
+        }
         break;
       case 'down':
-        cmd += " -s 'Tilt (relative)' -- 100";
-        break;
-      case 'left':
-        cmd += " -s 'Pan (relative)' -- 100";
+        if (position.y > limit.y * -1) {
+          position.y -= 1;
+          cmd += " -s 'Tilt (relative)' -- " + step;
+        }
         break;
       case 'right':
-        cmd += " -s 'Pan (relative)' -- -100";
+        if (position.x < limit.x) {
+          position.x += 1;
+          cmd += " -s 'Pan (relative)' -- -" + step;
+        }
+        break;
+      case 'left':
+        if (position.x > limit.x * -1) {
+          position.x -= 1;
+          cmd += " -s 'Pan (relative)' -- " + step;
+        }
         break;
     }
+    console.log(position);
 
-    console.log(cmd);
+    console.log('CMD:' ,cmd);
 
-    child = exec(cmd, function (error, stdout, stderr) {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
-      if (error !== null) {
-        console.log('exec error: ' + error);
-        res.json({
-          'success': 'false',
-          'error': 'exec error: ' + error
-        }) 
-      } else {
-        res.json({
-          'success': 'true'
-        }) 
-      } 
-    }); 
+    if(cmd !== 'uvcdynctrl') {
+      child = exec(cmd, function (error, stdout, stderr) {
+        // console.log('stdout: ' + stdout);
+        // console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+          res.json({
+            'success': false,
+            'error': 'exec error: ' + error
+          }) 
+        } else {
+          res.json({
+            'success': true
+          }) 
+        } 
+      }); 
+    } else {
+      res.json({
+        'success': false,
+        'error': 'Limit reached',
+      }) 
+    }
   }
 });
 
@@ -102,6 +156,7 @@ var server = app.listen(8083, function () {
   var host = server.address().address;
   var port = server.address().port;
 
+  resetPosition(); 
   console.log('Example app listening at http://%s:%s', host, port);
 
 });
